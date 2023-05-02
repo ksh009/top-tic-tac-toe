0a311974867fb1b9f84dd28569f0aa9fb523ee7a
HughesSeanKyle <khughessean@yahoo.com>
2023-05-02 13:25:49 +0200

    game winning conditions

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const gameBoard = (() => {
 	// private variable to represent the gameboard array
-	let board = ['X', 'O', '', 'X', '', 'O', '', '', ''];
+	let board = ['X', 'O', 'O', 'X', 'O', 'O', 'X', 'X', 'O'];
 
 	// public function to render the gameboard to the DOM
 	const render = () => {
 		// select the table element in the DOM
 		const table = document.querySelector('table');
+		console.log('table', table);
+		console.log('table.rows', table.rows);
 
 		// loop through each cell of the table and fill it with the corresponding value from the gameboard array
 		for (let i = 0; i < table.rows.length; i++) {
@@ -23,8 +25,38 @@ const gameBoard = (() => {
 	};
 })();
 
-gameBoard.board = ['X', 'O', '', 'X', '', 'O', '', '', ''];
+gameBoard.board = ['X', 'O', 'O', 'X', 'X', 'O', 'X', 'X', 'O'];
 gameBoard.render();
+
+// Use function in bigger function later
+/*
+	Check winning conditions
+*/
+const checkWin = () => {
+	const winningCombinations = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+
+	for (let i = 0; i < winningCombinations.length; i++) {
+		const [a, b, c] = winningCombinations[i];
+		if (
+			gameBoard.board[a] !== '' &&
+			gameBoard.board[a] === gameBoard.board[b] &&
+			gameBoard.board[b] === gameBoard.board[c]
+		) {
+			return true;
+		}
+	}
+
+	return false;
+};
 
 /* OLD CODE
 // Game board and squares
