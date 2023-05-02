623156938801e1912005760be2c062421a7a0cb6
HughesSeanKyle <khughessean@yahoo.com>
2023-05-02 17:11:25 +0200

    reset board state and cleanup

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 const gameBoard = (() => {
-	// private variable to represent the gameboard array
-	let board = ['X', 'O', 'O', 'X', 'O', 'O', 'X', 'X', 'O'];
+	let board = ['', '', '', '', '', '', '', '', ''];
 
-	// public function to render the gameboard to the DOM
 	const render = () => {
-		// select the table element in the DOM
 		const table = document.querySelector('table');
-		console.log('table', table);
-		console.log('table.rows', table.rows);
 
-		// loop through each cell of the table and fill it with the corresponding value from the gameboard array
 		for (let i = 0; i < table.rows.length; i++) {
 			for (let j = 0; j < table.rows[i].cells.length; j++) {
 				table.rows[i].cells[j].textContent = board[i * 3 + j];
@@ -19,14 +13,10 @@ const gameBoard = (() => {
 		console.log('table', table);
 	};
 
-	// public API
 	return {
 		render,
 	};
 })();
-
-gameBoard.board = ['X', 'O', 'O', 'X', 'X', 'O', 'X', 'X', 'O'];
-gameBoard.render();
 
 // player factory function
 const createPlayer = (name, marker) => {
@@ -68,26 +58,3 @@ const checkWin = () => {
 
 	return false;
 };
-
-/* OLD CODE
-// Game board and squares
-const newGamebtn = document.getElementById('newGameBtn');
-const gameBtnText = document.querySelector('.game-btn-text');
-const board = document.querySelector('.board');
-const squares = document.querySelectorAll('.square');
-
-// Current player
-let currentPlayer = 'X';
-
-// Event listener for each square
-squares.forEach((square) => {
-	square.addEventListener('click', () => {
-		if (square.textContent === '') {
-			square.classList.add(currentPlayer);
-			square.textContent = currentPlayer;
-
-			currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-		}
-	});
-});
-*/
